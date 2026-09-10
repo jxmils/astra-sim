e2d83568841a2854daffb85529376916ef90c004
Jason Paul Miller <jasonversamiller@gmail.com>
2026-09-10 20:48:39 +0100

    Expose custom graph ECMP capability

--- astra-sim/network_frontend/htsim/HTSimMain.cc
+++ astra-sim/network_frontend/htsim/HTSimMain.cc
@@ -42,6 +42,7 @@ void print_backend_capabilities(const std::string& admission) {
         << " initial_ocs_state=cold"
         << " initial_ocs_reconfiguration=uniform"
         << " custom_link_latency=1"
+        << " custom_graph_ecmp=deterministic_equal_hop_pair_hash_v1"
         << " ocs_plane_latency=whole_path"
         << std::endl;
 }
